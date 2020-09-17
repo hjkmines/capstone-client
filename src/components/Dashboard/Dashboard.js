dbb80e0e745daae7e65444346c6b94616f986ba7
Hyung Kim <hjkmines@gmail.com>
2020-09-17 16:46:35 -0600

    adds dashboard 1

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AppBar from '../AppBar/AppBar'; 
 import DataTable from './DataTable'; 
 import NestedList from './NestedList'; 
+import Dashboard1 from './Dashboard1'; 
 
 const Dashboard = () => {
 
     return (
         <div>
             <AppBar /> 
+            <Dashboard1 />
             {/* <DataTable />  */}
             {/* <NestedList />  */}
             {/* <h1>Dashboard</h1> */}
---
+++ src/components/Dashboard/Dashboard1.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './Dashboard1.css'
+
+const Dashboard1 = () => {
+
+    return(
+        <div className='dashboard1'>
+
+        </div>
+    )
+}
+
+export default Dashboard1; 
