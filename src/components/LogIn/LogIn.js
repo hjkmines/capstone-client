499019c3356e67794240db868aad147c547682e4
Hyung Kim <hjkmines@gmail.com>
2020-09-16 20:19:06 -0600

    adds login component

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './LogIn.css'
+import Box from '@material-ui/core/box'
+
+const LogIn = () => {
+
+    return (
+        <div className='form'>
+        
+        </div>
+    )
+}
+
+export default LogIn; 
