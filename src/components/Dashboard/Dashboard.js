0bf5aa0ddde0704783c82ef75fcb7227c6aeff8a
Hyung Kim <hjkmines@gmail.com>
2020-09-16 22:53:02 -0600

    adds paginated table

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react'; 
 import AppBar from '../AppBar/AppBar'; 
+import DataTable from './DataTable'; 
 
 const Dashboard = () => {
 
     return (
         <div>
             <AppBar /> 
+            <DataTable /> 
+            <h1>Dashboard</h1>
         </div>
     )
 }; 
