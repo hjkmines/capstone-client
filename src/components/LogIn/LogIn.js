fa962002a8968e969e437fe82fcc68efe156d3e6
Hyung Kim <hjkmines@gmail.com>
2020-09-16 22:18:58 -0600

    adds final styling to form login

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -16,22 +16,34 @@ const LogIn = () => {
         <form 
             noValidate 
             autoComplete="off" 
-            className='login'>
+            className='login'
+        >
             <TextField 
                 id="standard-basic" 
                 label="Email" 
-                className='textfield'
+                className='textfield item'
                 />
             <TextField 
-            type="password" 
-            id="standard-basic" 
-            label="Email" 
-            className='textfield'
+                type="password" 
+                id="standard-basic" 
+                label="Password" 
+                className='textfield item'
+                style={{ marginTop: '20px' }}
             />
-            <Button variant="contained" color="primary">
+            <Button 
+                variant="contained" 
+                color="primary"
+                className='item formButton'
+                style={{ marginTop: '30px' }}
+            >
             Log In 
             </Button>
-            <Button variant="contained" color="primary">
+            <Button 
+                variant="contained" 
+                color="primary"
+                className='item formButton'
+                style={{ marginTop: '20px' }}
+            >
             Sign Up
             </Button>
         </form>
