15778aa6f7a9cd0460829192ad359a18c4c70512
Hyung Kim <hjkmines@gmail.com>
2020-09-16 20:45:37 -0600

    adds styles to log in page

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,12 +1,20 @@
 import React from 'react'; 
 import './LogIn.css'
-import Box from '@material-ui/core/box'
+import Box from '@material-ui/core/box';
+import logo from '../bugView.JPG';
+
 
 const LogIn = () => {
 
     return (
+        <div>
+            <img 
+                src={logo} 
+                alt="bugview logo" 
+                className="logo"
+            /> 
         <div className='form'>
-        
+        </div>
         </div>
     )
 }
